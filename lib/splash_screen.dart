6b7ac1c2c2ec054f46efe353d294bee7a9f87bae
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2021-11-17 00:45:10 +0300

    Just add photo and text in the center of App

---
+++ lib/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.red,
+              Colors.blue,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset("images/intro.png"),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "flutter",
+                style: TextStyle(fontSize: 60, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
