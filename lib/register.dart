79ea238d4e0030dfb96c2c28cd763be0d55e309d
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2021-11-18 14:50:02 +0300

    start working on registration

--- lib/register.dart
+++ lib/register.dart
@@ -1,17 +1,38 @@
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
-  const Register({ Key? key }) : super(key: key);
+  const Register({Key? key}) : super(key: key);
 
   @override
   _RegisterState createState() => _RegisterState();
 }
 
 class _RegisterState extends State<Register> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _passwordConfirmController =
+      TextEditingController();
+  final TextEditingController _phoneConfirmController = TextEditingController();
+  final TextEditingController _imageController = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              child: Padding(padding: EdgeInsets.all(15),child: ,),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
