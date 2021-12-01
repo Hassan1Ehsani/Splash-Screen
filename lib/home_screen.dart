1e465f9a48f345c27345789cf1b734e32269fcfb
Hassan1Ehsani <hassan.ehsani.am@gmail.com>
2021-12-01 15:55:37 +0300

    create the home Screen for user. after user logined user goes to the page home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const HomeScreen({ Key key }) : super(key: key);
+  const HomeScreen({Key key}) : super(key: key);
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -10,8 +10,63 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.refresh,
+              color: Colors.white,
+            )),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Icon(
+                Icons.person,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Icon(
+                Icons.login_outlined,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blueAccent, Colors.redAccent],
+            ),
+          ),
+        ),
+        title: Text("Home Page"),
+      ),
+      body: Center(
+        child: Container(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add Post',
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
